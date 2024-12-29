b09b752e56c390396ac0cd2105a79f3805d5d952
rosepose <rosepose@gmail.com>
2024-12-29 12:02:42 +0330

    latest version added

--- CA6/p1_1.m
+++ CA6/p1_1.m
@@ -1,2 +1,9 @@
+
+tStart = 0;
+tEnd = 1;
+fs = 100;
+fc = 5;
+signalLen = tStart : 1/fs : tEnd - 1/fs;
 signal1 = cos(2 * pi * fc * signalLen);
-signalPlotter(signal1);
+
+signalPlotter(signal1, signalLen);
--- CA6/signalPlotter.m
+++ CA6/signalPlotter.m
@@ -1,10 +1,4 @@
-function signalPlotter(signal)
-    tStart = 0;
-    tEnd = 1;
-    fs = 100;
-    
-    signalLen = tStart : 1/fs : tEnd - 1/fs;
-
+function signalPlotter(signal, signalLen)
     figure;
     plot(signalLen, signal);
     title("signal:");
